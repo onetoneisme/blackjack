e15ccc29ede123025707baa63dc1b6539c96de92
s.matyukevich <s.matyukevich@gmail.com>
2015-12-03 12:29:28 +0300

    default values fro some variables added

--- terraform-aws-bosh-lite-cf-bootstrap/variables.tf
+++ terraform-aws-bosh-lite-cf-bootstrap/variables.tf
@@ -2,10 +2,18 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_key_path" {}
 variable "aws_key_name" {}
-variable "aws_jumpbox_ami" {}
-variable "aws_bosh_lite_ami" {}
 
-variable "bosh_lite_stemcell" {}
+variable "aws_jumpbox_ami" {
+  default = "ami-d05e75b8"
+}
+
+variable "aws_bosh_lite_ami" {
+  default = "ami-ec4e3186"
+}
+
+variable "bosh_lite_stemcell" {
+  default = "https://s3.amazonaws.com/bosh-warden-stemcells/bosh-stemcell-3126-warden-boshlite-ubuntu-trusty-go_agent.tgz"
+}
 
 variable "aws_jumpbox_instance_type" {
   default = "m4.large"
