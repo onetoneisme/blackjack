7fa81540fd18ffef0ff514f7a864ba69f71ce5ef
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-04-12 15:46:49 -0300

    Add content

--- units_refactored/CF for developers/Intermediate/001_Manifests/Prerequisites.sh
+++ units_refactored/CF for developers/Intermediate/001_Manifests/Prerequisites.sh
@@ -4,3 +4,4 @@ check 'curl -I --silent http://api.$CF_DOMAIN/v2/info -H "Host: api.$CF_DOMAIN" 
 check 'cf --version' 'cf version 6\.1.*' true #> Cloud Foundry CLI not installed or version outdated.
 check 'git --version' 'git version.*' true #> git is not installed.
 check 'cf apps' 'my-app.*started' true #> "my-app" application not found or not running.
+check 'cf services' 'my-app-db' true #> Service 'my-app-db' wasn't found.
