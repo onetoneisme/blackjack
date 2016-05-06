b19d013494a6756090a2519d94ceba853a95ee7a
s.matyukevich <s.matyukevich@gmail.com>
2016-05-06 13:02:44 +0300

    verify script in cf deployment lesson fixed

--- units_refactored/CF for DevOps engineers/Basic/002 Install Cloud Foundry/02_Prepare deployment manifest.verify.sh
+++ units_refactored/CF for DevOps engineers/Basic/002 Install Cloud Foundry/02_Prepare deployment manifest.verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-check 'cat ~/deployment/minimal-aws.yml | grep REPLACE' '^$' true #> all required fields in manifest should be replaced  
+check 'cat ~/deployment/minimal-aws.yml | grep REPLACE --color=never' '^$' true #> all required fields in manifest should be replaced  
