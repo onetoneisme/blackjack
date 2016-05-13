b6a43b67f01d092d394499e83a224d399d6820fb
s.matyukevich <s.matyukevich@gmail.com>
2016-05-13 13:52:34 +0300

    verify script fixed

--- units_refactored/CF for developers/Basic/001 Application Management in Cloud Foundry - Part 1/02_Your first application.verify.sh
+++ units_refactored/CF for developers/Basic/001 Application Management in Cloud Foundry - Part 1/02_Your first application.verify.sh
@@ -2,5 +2,5 @@
 
 check 'cf target | grep --color=never "API endpoint"' '.*.*API version.*.*' true #> Cloud Foundry is not set or is set incorrectly.
 check 'cf target | grep --color=never "User"' '^$' false #> You are not logged in.
-check 'cf target | grep --color=never "Org"' '^$' true #> Org is not selected.
-check 'cf target | grep --color=never "Space"' '^$' true #> Space is not selected.
+check 'cf target | grep --color=never "Org"' '^$' false #> Org is not selected.
+check 'cf target | grep --color=never "Space"' '^$' false #> Space is not selected.
