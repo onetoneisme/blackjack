5e03c60e03e0229d77addd21bd42a58ce0f90663
s-matyukevich <s.matyukevich@gmail.com>
2016-05-15 14:56:59 +0300

    ruby version updated

--- units_refactored/CF for DevOps engineers/Basic/001 Install BOSH with bosh-init/01_Install bosh-init.verify.sh
+++ units_refactored/CF for DevOps engineers/Basic/001 Install BOSH with bosh-init/01_Install bosh-init.verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 check 'bosh-init -v' '0.0.90' true #> bosh-init not installed or version outdated.
-check 'ruby --version' '2.3.*' true #> ruby not installed or version outdated.
+check 'ruby --version' '2.1.*' true #> ruby not installed or version outdated.
