3f5832dda1ec7ef8acd7aea2772fabb54e9dfc50
Victoria Kotova <viktoriya.k@altoros.com>
2017-03-06 21:33:53 +0300

    changed version of BOSH

--- units/cf_for_devops_engineers_advanced/install_cloud_foundry/prerequisites.sh
+++ units/cf_for_devops_engineers_advanced/install_cloud_foundry/prerequisites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 source .profile
-check 'bosh target' 'Current target is .*' true #> BOSH target should be set.
+check 'bosh target' 'Current target is 2.*' true #> BOSH target should be set.
