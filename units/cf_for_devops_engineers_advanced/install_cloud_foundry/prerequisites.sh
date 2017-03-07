08773c8724b4275632bc2602201c5f7b63fa4e0b
Victoria Kotova <viktoriya.k@altoros.com>
2017-03-07 16:56:35 +0300

    Change validation script

--- units/cf_for_devops_engineers_advanced/install_cloud_foundry/prerequisites.sh
+++ units/cf_for_devops_engineers_advanced/install_cloud_foundry/prerequisites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 source .profile
-check 'bosh target' 'Current target is .*' true #> BOSH target should be set.
+check 'bosh env' 'my-bosh' true #> You should log in to bosh director
