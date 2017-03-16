a157fc3af95ecfdffbfd7856837527f8c357971d
s-matyukevich <s.matyukevich@gmail.com>
2017-03-16 14:03:28 -0600

    validation scritps updated

--- units/quick_start/001_quick_start/target_you_org_and_space.verify.sh
+++ units/quick_start/001_quick_start/target_you_org_and_space.verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-check 'cf target | grep --color=never "Org"' $CF_ORG true #> Org not targeted correctly.
-check 'cf target | grep --color=never "Space"' $CF_SPACE true #> Space not targeted correctly.
+check 'cf target' $CF_ORG true #> Org not targeted correctly.
+check 'cf target' $CF_SPACE true #> Space not targeted correctly.
